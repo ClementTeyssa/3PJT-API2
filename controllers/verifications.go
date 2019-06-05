e457b62970e3ef4e8cc46c252fd50d77d212f50e
clement <292621@supinfo.com>
2019-06-05 14:24:41 +0200

    choose 3 nodes

--- controllers/verifications.go
+++ controllers/verifications.go
@@ -194,12 +194,14 @@ func getNodes() (NodesGet, error) {
 
 func choose3Nodes(pNodes Nodes) Nodes {
 	indexNode1 := rand.Intn(len(pNodes))
+
 	indexNode2 := rand.Intn(len(pNodes))
-	for ok := true; ok; ok = indexNode2 != indexNode1 {
+	for indexNode2 != indexNode1 {
 		indexNode2 = rand.Intn(len(pNodes))
 	}
+
 	indexNode3 := rand.Intn(len(pNodes))
-	for ok := true; ok; ok = indexNode2 != indexNode1 && indexNode3 != indexNode1 {
+	for indexNode2 != indexNode1 && indexNode3 != indexNode1 {
 		indexNode3 = rand.Intn(len(pNodes))
 	}
 
